21e9d4f0843a10ae44356a09e6512ca2a730c56e
Wes Brewer <brewerwh@ornl.gov>
2023-01-04 16:24:25 -0500

    Cleanup and add usage comment

--- models/main.py
+++ models/main.py
@@ -1,3 +1,4 @@
+"""usage: python main.py medium"""
 import argparse
 import importlib
 import numpy as np
@@ -6,8 +7,6 @@ import tensorflow as tf
 
 from tensorflow import keras
 from tensorflow.keras import layers
-
-#from medium import build_model
 
 archs = [s.split('.')[0] for s in os.listdir('archs') if s[0:1] != '_']
 print(archs)
@@ -21,31 +20,20 @@ samples = 100
 epochs = 5
 batch_size = 32
 
-# compute synthetic data for X and y (0's and 1's)
+# compute synthetic data for X and Y
 input_shape = (101, 82, 9)
 output_shape = (101, 82)
 X = np.random.rand(samples, *input_shape)
-print(X.shape)
 Y = np.random.rand(samples, *output_shape)
 
-# Define architecture
-units = 512
-act_func = 'selu'
-#layer_func = layers.Dense
-#model = keras.Sequential()
-#model.add(layer_func(units, activation=act_func, input_shape=(num_feats,)))
-#model.add(layer_func(units, activation=act_func))
-#model.add(layer_func(units, activation=act_func))
-#model.add(layers.Dense(1, activation='sigmoid'))
-
-#model = build_model(input_shape)
+# define model
 model = importlib.import_module('archs.' + args.arch).build_model(input_shape)
 model.summary()
 
-# Compile model
+# compile model
 model.compile(loss='mae', optimizer='adam')
 
-# Train model
+# train model
 model.fit(X, Y, batch_size=batch_size, epochs=epochs)
 
 model.save('mymodel')
