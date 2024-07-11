30d71c51db4387f2e7ab950c7f9946f6e79e1010
Gregor von Laszewski <laszewski@gmail.com>
2024-07-11 11:28:54 -0400

    switch traning to use config.yaml file

--- target/ubuntu-docker/smartsim/train.orig.py
+++ target/ubuntu-docker/smartsim/train.orig.py
@@ -1,6 +1,5 @@
 """usage: python train.py {small_lstm|medium_cnn|large_tcnn}"""
-import sys
-# import argparse
+import argparse
 import importlib
 import numpy as np
 import os
@@ -11,47 +10,27 @@ from cloudmesh.common.StopWatch import StopWatch
 from cloudmesh_pytorchinfo import print_gpu_device_properties
 from cloudmesh.gpu.gpu import Gpu
 from pprint import pprint
-from cloudmesh.common.FlatDict import FlatDict
-from cloudmesh.common.console import Console
-
 
 StopWatch.start("init")
-
-config = FlatDict()
-config.load("config.yaml")
-
-print(config)
-
-# check experiment parameters
-
-terminate = False
-for key in ["experiment.arch",
-            "experiment.samples", 
-            "experiment.epochs", 
-            "experiment.batch_size"]:
-    if key not in config:
-        Console.error(f"{key} not defined in config.yaml")
-        terminate = True
-
-if terminate:
-    sys.exit()
-
-
+parser = argparse.ArgumentParser()
+archs = [s.split('.')[0] for s in os.listdir('archs') if s[0:1] != '_']
+parser.add_argument('arch', type=str, choices=archs, help='Type of neural network architectures')
+parser.add_argument('-b', '--batch', type=int, default=1, help='batch size')
+args = parser.parse_args()
 
 # Parameters
-samples = int(config["experiment.samples"])
-epochs = int(config["experiment.epochs"])
-batch_size = int(config["experiment.batch_size"])    
-arch = config["experiment.arch"]
+samples = 100
+epochs = 5
+batch_size = args.batch
 
 # Compute synthetic data for X and Y
-if arch == "small_lstm":
+if args.arch == "small_lstm":
     input_shape = (8, 48)  # Sequence length, feature size
     output_shape = (24,)  # Total output size
-elif arch == "medium_cnn":
+elif args.arch == "medium_cnn":
     input_shape = (9, 101, 82)  # Channels, Height, Width
     output_shape = (101*82,)  # Flattened output size
-elif arch == "large_tcnn":
+elif args.arch == "large_tcnn":
     input_shape = (9, 101, 82)  # Channels, Depth, Height, Width for 3D CNNs, but let's simplify
     output_shape = (101*82,)  # Adjust based on actual model architecture
 else:
@@ -85,7 +64,7 @@ dataset = TensorDataset(X_tensor, Y_tensor)
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 # Define model
-model_module = importlib.import_module(f'archs.{arch}')
+model_module = importlib.import_module('archs.' + args.arch)
 model = model_module.build_model(input_shape)
 print(model)
 
@@ -122,13 +101,10 @@ model.eval()
 
 example_input = torch.rand(1, *input_shape, device=device_name)
 scripted_model = torch.jit.trace(model, example_input)
-scripted_model.save(f"{arch}_model.jit")
+scripted_model.save(f"{args.arch}_model.jit")
 
 
 
 StopWatch.stop("save")
 
-tag = f"arch={arch},samples={samples},epochs={epochs},batch_size={batch_size}"
-
-StopWatch.benchmark(tag=tag, 
-                    attributes=["timer", "time", "start", "tag", "msg"])
+StopWatch.benchmark()
