202b02be141c4658f3cfb355212edb7db6d1abb9
Gregor von Laszewski <laszewski@gmail.com>
2024-08-13 17:44:03 -0400

    improve the cm slurm template

--- target/rivanna/smartsim/bench-train.in.sh
+++ target/rivanna/smartsim/bench-train.in.sh
@@ -1,15 +1,41 @@
-#SBATCH
+#!/usr/bin/env bash
 
+#SBATCH --job-name={identifier}
+#SBATCH --output=osmi-{identifier}-%u-%j.out
+#SBATCH --error=osmi-{identifier}-%u-%j.err
+{slurm.sbatch}
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=64GB
+#SBATCH --cpus-per-task=1
+#SBATCH --time={ee.time}
+
+WORKDIR=/scratch/$USER/osmi-bench-new/target/rivanna/smartsim
+
+CONTAINER=$WORKDIR/images/cloudmesh-smartsim.sif
+
+PROGRESS() {
+    echo "# ###############################################"
+    echo "# cloudmesh status=$1 progress=$2 msg=$3 pid=$$"
+    echo "# ###############################################"
+}
+
+PROGRESS "running" "configure" 1
 module load apptainer
 module load cuda/12.4.1
 
-APPTAINER_IMAGE=cloudmesh-smartsim
-UID=`id -u`
-GID=`id -g`
-CODE=/home/green/Desktop/osmi-bench-new/target/rivanna/smartsim
+PROGRESS "running" "pwd" 2
 
 pwd
 
-apptainer run --nv  -it cloudmesh/smartsim bash -c "python3 train.py"
+
+echo "============================================================"
+echo "PROJECT_ID: {identifier}"
+echo "REPEAT: {experiment.repeat}"
+
+PROGRESS "running" "training" 3
+PROGRESS "running" "train" 2
+
+apptainer run --nv  $CONTAINER bash -c "python3 train.py"
 
 # make train
